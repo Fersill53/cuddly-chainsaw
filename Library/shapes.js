195d344ca1e20820426f51a309bcf1ba3f97f1be
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-07-10 20:58:42 -0500

    added shape class to shapes.js

--- Library/shapes.js
+++ Library/shapes.js
@@ -3,3 +3,36 @@ class shape {
         this.color = color
     }
 }
+
+class Triangle extends shape {
+    constructor(color) {
+        super(color);
+        this.name = 'Triangle';
+    }
+
+    draw() {
+        return `<polygon points="50,0 0,100 100,100" fill="${this.color}" />`;
+    }
+}
+
+class Circle extends shape {
+    constructor(color) {
+        super(color);
+        this.name = 'Circle';
+    }
+
+    draw() {
+        return `<circle cx="50" cy="50" r="50" fill="${this.color}" />`;
+    }
+}
+
+class Square extends shape {
+    constructor(color) {
+        super(color);
+        this.name = 'Square';
+    }
+
+    draw() {
+        return `<rect width="100" height="100" fill="${this.color}" />`;
+    }
+}
