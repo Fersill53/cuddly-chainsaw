54101214e96880d7e76b78dd8df3f2f759efd2b6
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-07-12 19:29:07 -0500

    retrying shapes tests

--- Library/shapes.test.js
+++ Library/shapes.test.js
@@ -1,7 +1,48 @@
 
-const {Triangle, Circle, Square} = require("../Library/shapes");
+// const {Triangle, Circle, Square} = require("../Library/shapes");
 
-test('Triangle should have red background', () => {
-    const triangle = new Triangle('red');
-    expect(triangle.render()).toBe('<polygon points=\"50,0 0,100 100,100\" fill=\"red"\ />');
+// test('Triangle should have red background', () => {
+//     const triangle = new Triangle('red');
+//     expect(triangle.render()).toBe('<polygon points=\"50,0 0,100 100,100\" fill=\"red"\ />');
+// });
+
+const {Circle, Square, Triangle} = require("../Library/shapes");
+
+describe('Circle', () => {
+    test('render() should generate SVG with circle and text', () => {
+      const circle = new Circle('red');
+      const text = 'Test Text';
+      const expectedSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+        <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+        <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-size="20" fill="black">${text}</text>
+      </svg>`;
+  
+      expect(circle.render(text)).toBe(expectedSVG);
+    });
+  });
+
+  describe('Square', () => {
+    test('render() should generate SVG with square and text', () => {
+      const square = new Square('blue');
+      const text = 'Test Text';
+      const expectedSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+        <rect x="10" y="10" width="80" height="80" stroke="black" stroke-width="3" fill="blue" />
+        <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-size="20" fill="black">${text}</text>
+      </svg>`;
+  
+      expect(square.render(text)).toBe(expectedSVG);
+    });
+  });
+
+  describe('Triangle', () => {
+    test('render() should generate SVG with triangle and text', () => {
+      const triangle = new Triangle('green');
+      const text = 'Test Text';
+      const expectedSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height=100">
+      <polygon points="50,10 90,90 10,90" stroke="black" stroke-width="3" fill="green" />
+      <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-size="20" fill="black">${text}</text>
+    </svg>`;
+
+    expect(triangle.render(text)).toBe(expectedSVG);
+  });
 });
